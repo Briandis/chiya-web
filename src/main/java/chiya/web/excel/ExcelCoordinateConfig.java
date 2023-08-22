0bbfee15cb1bb8b45f9e593ca5cc1f65c397ab2c
chiya <wei895926300@126.com>
2023-08-22 15:49:16 +0800

    针对excel导出新增模板配置导出模式

---
+++ src/main/java/chiya/web/excel/ExcelCoordinateConfig.java
@@ -0,0 +1,339 @@
+package chiya.web.excel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Excel坐标配置
+ * 
+ * @author chiya
+ *
+ */
+public class ExcelCoordinateConfig {
+
+	/** 行坐标 */
+	private Integer rowIndex = 0;
+	/** 列坐标 */
+	private Integer cellIndex = 0;
+	/** 引用的值 */
+	private String reference;
+	/** 默认单元格填充的值 */
+	private String defalutValue;
+	/** 对引用的值的格式化的方法 */
+	private String format;
+	/** 标注类型 */
+	private String type;
+
+	/** 特殊规则下，一行几个 */
+	private Integer lineCount;
+	/** 特殊规则下，间距 */
+	private Integer spacing;
+	/** 所在sheet名称 */
+	private String sheetName;
+
+	/** 深层的相对坐标 */
+	private List<ExcelCoordinateConfig> listExcelCoordinateConfig;
+
+	/**
+	 * 添加坐标配置
+	 * 
+	 * @param excelCoordinateConfig 坐标配置
+	 * @return 自身
+	 */
+	public ExcelCoordinateConfig chainConfig(ExcelCoordinateConfig excelCoordinateConfig) {
+		if (listExcelCoordinateConfig == null) { listExcelCoordinateConfig = new ArrayList<>(); }
+		listExcelCoordinateConfig.add(excelCoordinateConfig);
+		return this;
+	}
+
+	/**
+	 * 获取行坐标
+	 * 
+	 * @return 行坐标
+	 */
+	public Integer getRowIndex() {
+		return rowIndex;
+	}
+
+	/**
+	 * 设置行坐标
+	 * 
+	 * @param rowIndex 行坐标
+	 */
+	public void setRowIndex(Integer rowIndex) {
+		this.rowIndex = rowIndex;
+	}
+
+	/**
+	 * 链式添加行坐标
+	 * 
+	 * @param rowIndex 行坐标
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainRowIndex(Integer rowIndex) {
+		setRowIndex(rowIndex);
+		return this;
+	}
+
+	/**
+	 * 获取列坐标
+	 * 
+	 * @return 列坐标
+	 */
+	public Integer getCellIndex() {
+		return cellIndex;
+	}
+
+	/**
+	 * 设置列坐标
+	 * 
+	 * @param cellIndex 列坐标
+	 */
+	public void setCellIndex(Integer cellIndex) {
+		this.cellIndex = cellIndex;
+	}
+
+	/**
+	 * 链式添加列坐标
+	 * 
+	 * @param cellIndex 列坐标
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainCellIndex(Integer cellIndex) {
+		setCellIndex(cellIndex);
+		return this;
+	}
+
+	/**
+	 * 获取引用的值
+	 * 
+	 * @return 引用的值
+	 */
+	public String getReference() {
+		return reference;
+	}
+
+	/**
+	 * 设置引用的值
+	 * 
+	 * @param reference 引用的值
+	 */
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	/**
+	 * 链式添加引用的值
+	 * 
+	 * @param reference 引用的值
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainReference(String reference) {
+		setReference(reference);
+		return this;
+	}
+
+	/**
+	 * 获取默认单元格填充的值
+	 * 
+	 * @return 默认单元格填充的值
+	 */
+	public String getDefalutValue() {
+		return defalutValue;
+	}
+
+	/**
+	 * 设置默认单元格填充的值
+	 * 
+	 * @param defalutValue 默认单元格填充的值
+	 */
+	public void setDefalutValue(String defalutValue) {
+		this.defalutValue = defalutValue;
+	}
+
+	/**
+	 * 链式添加默认单元格填充的值
+	 * 
+	 * @param defalutValue 默认单元格填充的值
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainDefalutValue(String defalutValue) {
+		setDefalutValue(defalutValue);
+		return this;
+	}
+
+	/**
+	 * 获取对引用的值的格式化的方法
+	 * 
+	 * @return 对引用的值的格式化的方法
+	 */
+	public String getFormat() {
+		return format;
+	}
+
+	/**
+	 * 设置对引用的值的格式化的方法
+	 * 
+	 * @param format 对引用的值的格式化的方法
+	 */
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+	/**
+	 * 链式添加对引用的值的格式化的方法
+	 * 
+	 * @param format 对引用的值的格式化的方法
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainFormat(String format) {
+		setFormat(format);
+		return this;
+	}
+
+	/**
+	 * 获取深层的相对坐标
+	 * 
+	 * @return 深层的相对坐标
+	 */
+	public List<ExcelCoordinateConfig> getListExcelCoordinateConfig() {
+		return listExcelCoordinateConfig;
+	}
+
+	/**
+	 * 设置深层的相对坐标
+	 * 
+	 * @param listExcelCoordinateConfig 深层的相对坐标
+	 */
+	public void setListExcelCoordinateConfig(List<ExcelCoordinateConfig> listExcelCoordinateConfig) {
+		this.listExcelCoordinateConfig = listExcelCoordinateConfig;
+	}
+
+	/**
+	 * 链式添加深层的相对坐标
+	 * 
+	 * @param listExcelCoordinateConfig 深层的相对坐标
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainListExcelCoordinateConfig(List<ExcelCoordinateConfig> listExcelCoordinateConfig) {
+		setListExcelCoordinateConfig(listExcelCoordinateConfig);
+		return this;
+	}
+
+	/**
+	 * 获取标注类型
+	 * 
+	 * @return 标注类型
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * 设置标注类型
+	 * 
+	 * @param type 标注类型
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * 链式添加标注类型
+	 * 
+	 * @param type 标注类型
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainType(String type) {
+		setType(type);
+		return this;
+	}
+
+	/**
+	 * 获取特殊规则下，一行几个
+	 * 
+	 * @return 特殊规则下，一行几个
+	 */
+	public Integer getLineCount() {
+		return lineCount;
+	}
+
+	/**
+	 * 设置特殊规则下，一行几个
+	 * 
+	 * @param lineCount 特殊规则下，一行几个
+	 */
+	public void setLineCount(Integer lineCount) {
+		this.lineCount = lineCount;
+	}
+
+	/**
+	 * 链式添加特殊规则下，一行几个
+	 * 
+	 * @param lineCount 特殊规则下，一行几个
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainLineCount(Integer lineCount) {
+		setLineCount(lineCount);
+		return this;
+	}
+
+	/**
+	 * 获取特殊规则下，间距
+	 * 
+	 * @return 特殊规则下，间距
+	 */
+	public Integer getSpacing() {
+		return spacing;
+	}
+
+	/**
+	 * 设置特殊规则下，间距
+	 * 
+	 * @param spacing 特殊规则下，间距
+	 */
+	public void setSpacing(Integer spacing) {
+		this.spacing = spacing;
+	}
+
+	/**
+	 * 链式添加特殊规则下，间距
+	 * 
+	 * @param spacing 特殊规则下，间距
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainSpacing(Integer spacing) {
+		setSpacing(spacing);
+		return this;
+	}
+
+	/**
+	 * 获取所在sheet名称
+	 * 
+	 * @return 所在sheet名称
+	 */
+	public String getSheetName() {
+		return sheetName;
+	}
+
+	/**
+	 * 设置所在sheet名称
+	 * 
+	 * @param sheetName 所在sheet名称
+	 */
+	public void setSheetName(String sheetName) {
+		this.sheetName = sheetName;
+	}
+
+	/**
+	 * 链式添加所在sheet名称
+	 * 
+	 * @param sheetName 所在sheet名称
+	 * @return 对象本身
+	 */
+	public ExcelCoordinateConfig chainSheetName(String sheetName) {
+		setSheetName(sheetName);
+		return this;
+	}
+
+}
--- src/main/java/chiya/web/excel/ExcelUtil.java
+++ src/main/java/chiya/web/excel/ExcelUtil.java
@@ -7,15 +7,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import chiya.core.base.collection.ContainerUtil;
 import chiya.core.base.exception.Assert;
+import chiya.core.base.loop.Loop;
+import chiya.core.base.number.NumberUtil;
+import chiya.core.base.pack.IntegerPack;
+import chiya.core.base.string.StringUtil;
 
 /**
  * Excel工具库
@@ -111,9 +117,9 @@ public class ExcelUtil {
 	 */
 	public static <T> void createTableHead(Sheet sheet, LinkedHashMap<String, Function<T, Object>> map) {
 		Row row = sheet.createRow(0);
-		AtomicInteger atomicInteger = new AtomicInteger(0);
+		IntegerPack integerPack = new IntegerPack(0);
 		map.forEach((key, value) -> {
-			row.createCell(atomicInteger.getAndIncrement()).setCellValue(key);
+			row.createCell(integerPack.getAndIncrement()).setCellValue(key);
 		});
 	}
 
@@ -128,17 +134,17 @@ public class ExcelUtil {
 	public static <T> void createTableBody(Sheet sheet, List<T> list, BaseFormatter<T> baseFormatter) {
 		HashMap<Integer, Integer> count = new HashMap<Integer, Integer>();
 
-		AtomicInteger atomicInteger = new AtomicInteger(0);
+		IntegerPack integerPack = new IntegerPack(0);
 		list.forEach(obj -> {
-			Row row = sheet.createRow(atomicInteger.incrementAndGet());
-			AtomicInteger cellIndex = new AtomicInteger();
+			Row row = sheet.createRow(integerPack.incrementAndGet());
+			IntegerPack cellIndex = new IntegerPack();
 			baseFormatter.fieldConfig.forEach((key, value) -> {
 				// 单元默认格宽度
-				if (!count.containsKey(cellIndex.intValue())) { count.put(cellIndex.intValue(), 10); }
+				if (!count.containsKey(cellIndex.getData())) { count.put(cellIndex.getData(), 10); }
 				Object data = value.apply(obj);
 				String v = baseFormatter.formatter(key, data);
 				// 设置字符长度
-				if (v != null && count.get(cellIndex.intValue()) < v.length()) { count.put(cellIndex.intValue(), v.length()); }
+				if (v != null && count.get(cellIndex.getData()) < v.length()) { count.put(cellIndex.getData(), v.length()); }
 				row.createCell(cellIndex.getAndIncrement()).setCellValue(v);
 			});
 		});
@@ -203,5 +209,217 @@ public class ExcelUtil {
 		List<T> list = importExecl(sheet, excelRow, startRow);
 		ExcelUtil.workbookClose(workbook);
 		return list;
+	}
+
+	/**
+	 * 获取或创建一个sheet页
+	 * 
+	 * @param workbook 工作簿
+	 * @param name     sheet名称
+	 * @return sheet对象
+	 */
+	public static Sheet getOrCreateSheet(Workbook workbook, String name) {
+		Sheet sheet = workbook.getSheet(name);
+		if (sheet == null) { sheet = workbook.createSheet(name); }
+		return sheet;
+	}
+
+	/**
+	 * 向指定单元格中写入数据
+	 * 
+	 * @param sheet     sheet页
+	 * @param rowIndex  row坐标，从0计算
+	 * @param CellIndex cell坐标，从0计算
+	 * @param value     写入的对象
+	 */
+	public static void writeValue(Sheet sheet, int rowIndex, int CellIndex, Object value) {
+		Row row = sheet.getRow(rowIndex);
+		if (row == null) { row = sheet.createRow(rowIndex); }
+		Cell cell = row.getCell(CellIndex);
+		if (cell == null) { cell = row.createCell(CellIndex); }
+
+		if (value != null) { cell.setCellValue(value.toString()); }
+
+	}
+
+	/**
+	 * 向指定单元格中写入数据
+	 * 
+	 * @param sheet     sheet页
+	 * @param rowIndex  row坐标，从0计算
+	 * @param CellIndex cell坐标，从0计算
+	 * @param value     写入的对象
+	 */
+	public static void writeValue(Sheet sheet, int rowIndex, int CellIndex, List<Object> value) {
+		if (value != null) {
+			Loop.forEach(value, (obj, index) -> {
+				if (obj != null) { writeValue(sheet, rowIndex + index, CellIndex, obj.toString()); }
+			});
+		}
+	}
+
+	/**
+	 * 获取行的样式
+	 * 
+	 * @param row 行信息
+	 * @return 行每个单元格的样式
+	 */
+	public static HashMap<Integer, CellStyle> getRowStyle(Row row) {
+		HashMap<Integer, CellStyle> hashMap = new HashMap<>();
+		if (row != null) { row.forEach(cell -> hashMap.put(cell.getColumnIndex(), cell.getCellStyle())); }
+		return hashMap;
+	}
+
+	/**
+	 * 设置改行的样式
+	 * 
+	 * @param row     样式
+	 * @param hashMap 每个单元格样式配置
+	 */
+	public static void setRowStyle(Row row, HashMap<Integer, CellStyle> hashMap) {
+		hashMap.forEach((index, cellStyle) -> {
+			if (cellStyle != null) {
+				Cell cell = row.getCell(index);
+				if (cell == null) { cell = row.createCell(index); }
+				cell.setCellStyle(cellStyle);
+			}
+		});
+	}
+
+	/**
+	 * 在指定行数前插入指定数量的空行，保留插入行的样式
+	 * 
+	 * @param start 参考行数
+	 * @param count 插入多少行
+	 * @param sheet 当前操作的sheet页
+	 */
+	public static void insertRow(int start, int count, Sheet sheet) {
+		HashMap<Integer, CellStyle> hashMap = getRowStyle(sheet.getRow(start));
+		if (sheet.getRow(start) == null) { return; }
+		sheet.shiftRows(start, sheet.getLastRowNum(), count);
+		for (int i = start; i < start + count; i++) {
+			Row row = sheet.getRow(i);
+			if (row == null) { row = sheet.createRow(i); }
+			setRowStyle(row, hashMap);
+		}
+	}
+
+	/**
+	 * sheet页写入数据
+	 * 
+	 * @param listConfit 坐标配置
+	 * @param dataMap    数据
+	 * @param sheet      所在sheet页
+	 */
+	public static void writeData(List<ExcelCoordinateConfig> listConfit, HashMap<String, List<Object>> dataMap, Sheet sheet) {
+		// 对配置进行排序，按照区块标记的行号排序
+		listConfit = ContainerUtil.listSort(listConfit, (o1, o2) -> {
+			IntegerPack a = new IntegerPack(-1);
+			IntegerPack b = new IntegerPack(-1);
+			if (!StringUtil.eqString(o1.getType(), "flow")) {
+				o1.getListExcelCoordinateConfig().forEach(obj -> {
+					if (a.getData() == -1) { a.setData(obj.getRowIndex()); }
+					if (obj.getRowIndex() < a.getData()) { a.setData(obj.getRowIndex()); }
+				});
+			} else {
+				a.setData(o1.getRowIndex());
+			}
+			if (!StringUtil.eqString(o2.getType(), "flow")) {
+				o2.getListExcelCoordinateConfig().forEach(obj -> {
+					if (b.getData() == -1) { b.setData(obj.getRowIndex()); }
+					if (obj.getRowIndex() < b.getData()) { b.setData(obj.getRowIndex()); }
+				});
+			} else {
+				b.setData(o2.getRowIndex());
+			}
+
+			return NumberUtil.compareSize(a.getData(), b.getData());
+		});
+		IntegerPack insertCount = new IntegerPack();
+		listConfit.forEach(config -> {
+			// 流式布局处理
+			if (StringUtil.eqString(config.getType(), "flow")) {
+				// 计算插入行数
+				int size = config.getListExcelCoordinateConfig().size();
+				int insertSize = size % config.getLineCount() != 0 ? size / config.getLineCount() : size / config.getLineCount() + 1;
+
+				// 先插入行数
+				insertRow(config.getRowIndex(), insertSize, sheet);
+
+				IntegerPack x = new IntegerPack(config.getRowIndex() + insertCount.getData());
+				IntegerPack y = new IntegerPack(config.getCellIndex());
+
+				Loop.forEach(config.getListExcelCoordinateConfig(), (block, count) -> {
+					// 每行数量达到设置上限后换行
+					if (count != 0 && count % config.getLineCount() == 0) {
+						x.incrementAndGet();
+						y.setData(config.getCellIndex());
+					}
+					block.getListExcelCoordinateConfig().forEach(excelConfig -> {
+						if (excelConfig.getReference() != null) {
+							List<Object> data = dataMap.get(excelConfig.getReference());
+							Assert.isTrue(data == null || data.size() == 0, excelConfig.getReference() + "的值不存在");
+							writeValue(sheet, x.getData(), y.getData(), data);
+
+						} else if (excelConfig.getDefalutValue() != null) {
+							// 如果不存在引用 则写入默认值
+							writeValue(sheet, x.getData(), y.getData(), excelConfig.getDefalutValue());
+						}
+						y.incrementAndGet();
+					});
+					y.addAndGet(config.getSpacing());
+				});
+				insertCount.addAndGet(insertSize);
+			} else {
+				// 普通情况
+				// 计算插入行数，需要找到起始的列并计算差值
+				IntegerPack insertSize = new IntegerPack();
+				IntegerPack start = new IntegerPack();
+				config.getListExcelCoordinateConfig().forEach(block -> {
+					if (block.getReference() != null) {
+						if (start.getData() < block.getRowIndex()) { start.setData(block.getRowIndex()); }
+						List<Object> data = dataMap.get(block.getReference());
+						if (data != null && data.size() + block.getRowIndex() > insertSize.getData()) { insertSize.setData(data.size() + block.getRowIndex()); }
+					}
+				});
+				insertSize.setData(insertSize.getData() - start.getData() - 1);
+				// 先插入行数
+				if (insertSize.getData() < 0) { insertSize.setData(0); }
+				insertRow(start.getData() + insertCount.getData(), insertSize.getData(), sheet);
+
+				Loop.forEach(config.getListExcelCoordinateConfig(), (block, count) -> {
+					if (block.getReference() != null) {
+						List<Object> data = dataMap.get(block.getReference());
+						Assert.isTrue(data == null || data.size() == 0, block.getReference() + "的值不存在");
+						writeValue(sheet, block.getRowIndex() + insertCount.getData(), block.getCellIndex(), data);
+					} else if (block.getDefalutValue() != null) {
+						// 根据当前配置写入，并重新计算位置
+						writeValue(sheet, block.getRowIndex() + insertCount.getData(), block.getCellIndex(), block.getDefalutValue());
+					}
+				});
+
+				insertCount.addAndGet(insertSize.getData());
+			}
+		});
+	}
+
+	/**
+	 * sheet页写入数据
+	 * 
+	 * @param listConfit 坐标配置
+	 * @param dataMap    数据
+	 * @param sheet      所在sheet页
+	 */
+	public static void writeData(List<ExcelCoordinateConfig> listConfit, HashMap<String, List<Object>> dataMap, Workbook workbook) {
+		HashMap<String, List<ExcelCoordinateConfig>> hashMap = new HashMap<>();
+		listConfit.forEach(config -> {
+			if (!hashMap.containsKey(config.getSheetName())) { hashMap.put(config.getSheetName(), new ArrayList<>()); }
+			hashMap.get(config.getSheetName()).addAll(config.getListExcelCoordinateConfig());
+		});
+		hashMap.forEach((sheetName, list) -> {
+			Sheet sheet = getOrCreateSheet(workbook, sheetName);
+			writeData(list, dataMap, sheet);
+		});
+
 	}
 }
